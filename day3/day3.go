a0d588e5f9164108361f175652856fb98a195a75
Jayesh Mistry <jayesh.ish.mistry@gmail.com>
2019-12-04 09:29:43 +1300

    Interim checkin

--- day3/day3.go
+++ day3/day3.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"math"
 	"strconv"
 
@@ -14,16 +13,76 @@ func main() {
 	logger.Info("max coordinates", zap.Int("max-x", maxX), zap.Int("max-y", maxY))
 	makeGrid(maxX, maxY)
 
-	centerY := len(grid) - 1
-	centerX := 0
+	centerY := len(grid) / 2
+	centerX := len(grid[0]) / 2
 
 	unravelWires(centerX, centerY)
 	scanGrid(centerX, centerY)
 
+	signalDelay(centerX, centerY)
+
 	logger.Info("shortest distance", zap.Float64("distance", distance))
-	for y := range grid {
-		fmt.Println(y, ":\t", grid[y])
+}
+
+func signalDelay(centerX, centerY int) {
+	interX, interY, delay := followWire(centerX, centerY, 0, wire1)
+
+}
+
+func followWire(centerX, centerY, delay int, wire []string) (int, int, int) {
+	curX := centerX
+	curY := centerY
+	foundIntersection := false
+
+	for _, movement := range wire {
+		direction, size := dirAndSize(movement)
+
+		switch direction {
+		case "R":
+			for x := curX + 1; x <= (curX + size); x++ {
+				delay++
+				if grid[curY][x] == 3 {
+					foundIntersection = true
+					break
+				}
+			}
+			curX += size
+
+		case "L":
+			for x := curX - 1; x >= (curX - size); x-- {
+				delay++
+				if grid[curY][x] == 3 {
+					foundIntersection = true
+					break
+				}
+			}
+			curX -= size
+		case "D":
+			for y := curY + 1; y <= (curY + size); y++ {
+				delay++
+				if grid[y][curX] == 3 {
+					foundIntersection = true
+					break
+				}
+			}
+			curY += size
+		case "U":
+			for y := curY - 1; y >= (curY - size); y-- {
+				delay++
+				if grid[y][curX] == 3 {
+					foundIntersection = true
+					break
+				}
+			}
+			curY -= size
+		}
+
+		if foundIntersection {
+			break
+		}
 	}
+
+	return curX, curY, delay
 }
 
 func scanGrid(centerX, centerY int) {
@@ -58,11 +117,25 @@ func unravelWires(centerX, centerY int) {
 }
 
 func unravelWire(centerX, centerY int, wire []string, val int) {
+	logger.Info(
+		"starting coordinates",
+		zap.Int("center-x", centerX),
+		zap.Int("center-y", centerY),
+	)
+
 	curX := centerX
 	curY := centerY
 
 	for _, movement := range wire {
 		direction, size := dirAndSize(movement)
+
+		logger.Info(
+			"current position",
+			zap.Int("curr-x", curX),
+			zap.Int("curr-y", curY),
+			zap.String("direction", direction),
+			zap.Int("size", size),
+		)
 
 		switch direction {
 		case "R":
